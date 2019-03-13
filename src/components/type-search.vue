3c4cc5f7f7f5884de7fb0c455a7fb475545eecc3
freshvictory <justin.renjilian@gmail.com>
2019-03-13 00:14:30 -0400

    Get rid of pesky warnings. Also, you can hit enter now!

--- src/components/type-search.vue
+++ src/components/type-search.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <div :class="$style.container">
-      <input v-model="type" type="text">
-      <button @click="search">Search</button>
+      <form @submit.prevent="search">
+        <input v-model="typeInput" type="text">
+        <button type="submit">Search</button>
+      </form>
     </div>
     <div v-if="results[0] && results[0].relationships">
       <h2>{{ results[0].name }} type pokemon</h2>
@@ -27,11 +29,18 @@ import { Type } from '../models/type';
 export default class TypeSearchResultsDecorator extends Vue {
   private searchResults: Type[] = [];
 
-  @Prop({ default: 'dark' })
-  private readonly type!: string;
+  private type: string = 'dark';
 
   get results(): Type[] {
     return this.searchResults;
+  }
+
+  get typeInput(): string {
+    return this.type;
+  }
+
+  set typeInput(type: string) {
+    this.type = type;
   }
 
   private api = new Api('https://pokemon-type-api.herokuapp.com');
